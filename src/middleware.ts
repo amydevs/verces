0f9b2fc8142d8d7d8bb4ce21e111cd78f24fdcbd
amy <ctrlalternativedelete@gmail.com>
2022-11-30 14:26:41 +1100

    changed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,12 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname+'/'+request.nextUrl.search;
     if (path.startsWith('/.well-known/')) {
-        return NextResponse.rewrite(new URL(`/api${path}`, request.url))
+        return NextResponse.rewrite(new URL(`/api${path}`, request.url));
     }
     if (path.startsWith('/users/')) {
-        return NextResponse.rewrite(new URL(`/api${path}`, request.url))
+        return NextResponse.rewrite(new URL(`/api${path}`, request.url));
     }
+    return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
