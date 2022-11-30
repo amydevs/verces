d573b175de80ccffe1388eacf6151f4ac25b1044
amy <ctrlalternativedelete@gmail.com>
2022-12-01 07:16:50 +1100

    fleshing out

--- src/pages/api/users/[id]/inbox.ts
+++ src/pages/api/users/[id]/inbox.ts
@@ -1,9 +1,14 @@
+import { env } from "env/server.mjs";
 import { type NextApiRequest, type NextApiResponse } from "next";
 
 const inbox = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed')
+  }
   res.status(200).json({
     "help": "inbox"
   });
+  env.HOST
 };
 
 export default inbox;
