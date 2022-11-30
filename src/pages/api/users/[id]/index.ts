f8e256af53b57ebf37e55335c354e808caec171f
amy <ctrlalternativedelete@gmail.com>
2022-12-01 05:25:33 +1100

    added actor generation

--- src/pages/api/users/[id]/index.ts
+++ src/pages/api/users/[id]/index.ts
@@ -1,14 +1,39 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { prisma } from "server/db/client";
+import crypto from "crypto";
+import { string } from "zod";
+import { env } from "env/server.mjs";
+
+const createActor = (name: string, domain: string, pubKey: string) => {
+  return {
+    '@context': [
+      'https://www.w3.org/ns/activitystreams',
+      'https://w3id.org/security/v1'
+    ],
+
+    'id': `https://${domain}/users/${name}`,
+    'type': 'Person',
+    'preferredUsername': `${name}`,
+    'inbox': `https://${domain}/users/${name}/inbox`,
+    'followers': `https://${domain}/users/${name}/followers`,
+
+    'publicKey': {
+      'id': `https://${domain}/users/${name}#main-key`,
+      'owner': `https://${domain}/users/${name}`,
+      'publicKeyPem': pubKey
+    }
+  };
+}
 
 const user = async (req: NextApiRequest, res: NextApiResponse) => {
   const id = req.query.id;
   if (typeof id !== 'string') {
     return res.status(400).send('Bad Request')
   }
-  const foundUserActor = await prisma.user.findFirst({
+  const foundUser = await prisma.user.findFirst({
     select: {
-      actor: true
+      keyPair: true,
+      name: true
     },
     where: {
       OR: {
@@ -17,10 +42,11 @@ const user = async (req: NextApiRequest, res: NextApiResponse) => {
       }
     }
   });
-  if (!foundUserActor) {
+  if (!foundUser?.keyPair?.publicKey || !foundUser?.name) {
     return res.status(404).send('Not Found')
   }
-  return res.status(200).json(foundUserActor.actor);
+  
+  return res.status(200).json(createActor(foundUser.name, env.HOST,foundUser.keyPair?.publicKey));
 };
 
 export default user;
