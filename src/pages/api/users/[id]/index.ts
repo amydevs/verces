3cf1c0af1933a03d71b5b088e4cfc9330e138c5f
amy <ctrlalternativedelete@gmail.com>
2022-11-30 14:22:56 +1100

    added actors

--- src/pages/api/users/[id]/index.ts
+++ src/pages/api/users/[id]/index.ts
@@ -1,9 +1,26 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
+import { prisma } from "server/db/client";
 
 const user = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({
-    "help": "help"
+  const id = req.query.id;
+  if (typeof id !== 'string') {
+    return res.status(400).send('Bad Request')
+  }
+  const foundUserActor = await prisma.user.findFirst({
+    select: {
+      actor: true
+    },
+    where: {
+      OR: {
+        id,
+        name: id
+      }
+    }
   });
+  if (!foundUserActor) {
+    return res.status(400).send('Not Found')
+  }
+  return res.status(200).json(foundUserActor.actor);
 };
 
 export default user;
