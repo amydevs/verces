5918bddc62c7f2d3b4181b0ca5411f09f6d268e1
amy <ctrlalternativedelete@gmail.com>
2022-11-30 15:11:49 +1100

    added webfinger

--- src/pages/api/.well-known/webfinger.ts
+++ src/pages/api/.well-known/webfinger.ts
@@ -1,21 +1,27 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
+import { prisma } from "../../../server/db/client";
 
 const webfinger = async (req: NextApiRequest, res: NextApiResponse) => {
-    const reference = req.query.reference;
+    let reference = req.query.reference;
     if (typeof reference !== 'string') {
         return res.status(400).send('Bad Response')
     }
-    return res.status(200).json({
-        "subject": "acct:alice@my-example.com",
-    
-        "links": [
-            {
-                "rel": "self",
-                "type": "application/activity+json",
-                "href": `https://${req.headers.host}/actor`
-            }
-        ]
-    });
+
+    const acct = 'acct:';
+    if (reference.startsWith(acct)) {
+        reference = reference.substring(acct.length)
+    }
+    const lastIndexOfAt = reference.lastIndexOf('@');
+    if (lastIndexOfAt !== -1) {
+        reference = reference.substring(0, lastIndexOfAt);
+    }
+
+    const foundWebFinger = await prisma.user.findFirst({ where: { name: reference } });
+    if (!foundWebFinger) {
+        return res.status(400).send('Not Found')
+    }
+
+    return res.status(200).json(foundWebFinger.webfinger);
 };
   
 export default webfinger;
