a87b677ace989f3d8fc80a38c1099d68828ba8a9
amy <ctrlalternativedelete@gmail.com>
2022-12-01 18:38:07 +1100

    added placeholders

---
+++ src/pages/api/parent/users/[userId]/following.ts
@@ -0,0 +1,7 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+const following = async (req: NextApiRequest, res: NextApiResponse) => {
+    res.send("WIP")
+};
+
+export default following;
--- src/pages/api/parent/users/[userId]/inbox.ts
+++ src/pages/api/parent/users/[userId]/inbox.ts
@@ -1,14 +1,7 @@
-import { env } from "env/server.mjs";
 import { type NextApiRequest, type NextApiResponse } from "next";
 
 const inbox = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') {
-    return res.status(405).send('Method Not Allowed')
-  }
-  res.status(200).json({
-    "help": "inbox"
-  });
-  env.HOST
+    res.send("WIP")
 };
 
 export default inbox;
