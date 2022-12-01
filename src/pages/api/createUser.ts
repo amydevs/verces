4048ad289d4e46837f5b24dbe914d573d91a83e5
amy <ctrlalternativedelete@gmail.com>
2022-12-01 13:46:59 +1100

    added testing distinct host

--- src/pages/api/createUser.ts
+++ src/pages/api/createUser.ts
@@ -3,17 +3,29 @@ import { type NextApiRequest, type NextApiResponse } from "next";
 import { prisma } from "../../server/db/client";
 
 const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  await prisma.user.create({
-    data: {
-        name: "help",
-    }
-  })
-  return await prisma.user.create({
-    data: {
-        name: "help",
-        host: "help.com"
-    }
-  })
+    await prisma.user.create({
+        data: {
+            name: "help",
+            host: "help.com"
+        }
+    })
+    await prisma.user.create({
+        data: {
+            name: "help",
+        }
+    })
+    await prisma.user.create({
+        data: {
+            name: "hel1p",
+            host: "help.com"
+        }
+    })
+    await prisma.user.create({
+        data: {
+            name: "help",
+        }
+    })
+    res.send('help')
 };
 
 export default examples;
