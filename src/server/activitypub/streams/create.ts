7643532afaad344fc319510973caff92669f9dd1
amy <ctrlalternativedelete@gmail.com>
2022-12-02 12:42:44 +1100

    added generate create

--- src/server/activitypub/streams/create.ts
+++ src/server/activitypub/streams/create.ts
@@ -1,15 +1,18 @@
 import { type IActivity } from "../type";
 
 export const generateCreate = (name: string, domain: string, note: IActivity) => {
+    const actor = `https://${domain}/users/${name}/statuses/${note.id}`;
     const createMessage: IActivity = {
         '@context': 'https://www.w3.org/ns/activitystreams',
-    
+
         'id': `${note.id}/activity`,
         'type': 'Create',
-        'actor': note.id,
-        'to': ['https://www.w3.org/ns/activitystreams#Public'],
-        'cc': [follower],
-    
-        'object': noteMessage
+        'actor': actor,
+        'attributedTo': actor,
+        'to': note.to,
+        'cc': note.cc,
+        'object': note,
+        
     }
+    return createMessage;
 }
