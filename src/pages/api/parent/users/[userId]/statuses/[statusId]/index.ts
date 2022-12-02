8840e10e478efaeec1580fabfacc8fc25b9ff17b
amy <ctrlalternativedelete@gmail.com>
2022-12-02 14:56:13 +1100

    refix 3

--- src/pages/api/parent/users/[userId]/statuses/[statusId]/index.ts
+++ src/pages/api/parent/users/[userId]/statuses/[statusId]/index.ts
@@ -23,7 +23,7 @@ const status = async (req: NextApiRequest, res: NextApiResponse) => {
     if (!foundStatus) {
         return res.status(404).send('Not Found')
     }
-    return res.send(generateNote(foundStatus.user.name, env.HOST, foundStatus));
+    return res.json(generateNote(foundStatus.user.name, env.HOST, foundStatus));
 };
 
 export default status;
