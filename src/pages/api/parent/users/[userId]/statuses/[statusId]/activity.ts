276a4a79ea189326e603096dad848904a2eb86f7
amy <ctrlalternativedelete@gmail.com>
2022-12-02 12:44:14 +1100

    added create

--- src/pages/api/parent/users/[userId]/statuses/[statusId]/activity.ts
+++ src/pages/api/parent/users/[userId]/statuses/[statusId]/activity.ts
@@ -1,7 +1,33 @@
+import { env } from "env/server.mjs";
 import { type NextApiRequest, type NextApiResponse } from "next";
+import { generateCreate } from "server/activitypub/streams/create";
+import { generateNote, statusInclude } from "server/activitypub/streams/note";
+import { prisma } from "server/db/client";
 
-const createActivity = async (req: NextApiRequest, res: NextApiResponse) => {
-    res.send("help")
-}
+const status = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { userId, statusId } = req.query;
+    if (typeof userId !== 'string' || typeof statusId !== 'string') {
+        return res.status(400).send('Bad Request')
+    }
+    const foundStatus = await prisma.status.findFirst({
+        include: {
+            user: true,
+            ...statusInclude.include
+        },
+        where: {
+            user: {
+                OR: {
+                    id: userId,
+                    name: userId
+                }
+            },
+            id: statusId
+        }
+    });
+    if (!foundStatus) {
+        return res.status(404).send('Not Found')
+    }
+    return  generateCreate(foundStatus.user.name, env.HOST, generateNote(foundStatus.user.name, env.HOST, foundStatus));
+};
 
-export default createActivity;
+export default status;
