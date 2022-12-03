74115472811df950a001c65f5c50c9a136bcd47e
amy <ctrlalternativedelete@gmail.com>
2022-12-03 23:38:54 +1100

    readded next cors

--- src/pages/api/[...trpc].ts
+++ src/pages/api/[...trpc].ts
@@ -5,20 +5,21 @@ import { createOpenApiNextHandler } from 'trpc-openapi';
 import { createContext } from "../../server/trpc/context";
 import { appRouter } from "../../server/trpc/router/_app";
 
+import NextCors from 'nextjs-cors';
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
 
   // Handle incoming OpenAPI requests
   return createOpenApiNextHandler({
     router: appRouter,
     createContext,
-    responseMeta: (meta) => {
-      return {
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
-      };
-    }
   })(req, res);
 };
 
