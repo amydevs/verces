9d52245ff95805624b9d68299b222502be1fcffe
amy <ctrlalternativedelete@gmail.com>
2022-12-04 03:08:40 +1100

    added uris

--- src/lib/uris.ts
+++ src/lib/uris.ts
@@ -15,26 +15,26 @@ export const getUserUri = (user: string, options = defaultParams) => {
 
 export const getInboxUri = (user?: string, options = defaultParams) => {
     if (user) {
-        return new URL('/inbox', getUserUri(user, options)).toString()
+        return new URL(`${getUserUri(user, options)}/inbox`).toString()
     }
     else {
         return new URL('/inbox', getIndexUri(options)).toString()
     }
 }
 export const getOutboxUri = (user: string, options = defaultParams) => {
-    return new URL('/inbox', getUserUri(user, options)).toString()
+    return new URL(`${getUserUri(user, options)}/outbox`).toString()
 }
 export const getFollowersUri = (user: string, options = defaultParams) => {
-    return new URL('/followers', getUserUri(user, options)).toString()
+    return new URL(`${getUserUri(user, options)}/followers`).toString()
 }
 export const getFollowingUri = (user: string, options = defaultParams) => {
-    return new URL('/following', getUserUri(user, options)).toString()
+    return new URL(`${getUserUri(user, options)}/following`).toString()
 }
 
 export const getStatusUri = (user: string, status: string, options = defaultParams) => {
-    return new URL(`/statuses/${status}`, getUserUri(user, options)).toString()
+    return new URL(`${getUserUri(user, options)}/statuses/${status}`).toString()
 }
 
 export const getStatusActivityUri = (user: string, status: string, options = defaultParams) => {
-    return new URL(`/activity`, getStatusUri(user, status, options)).toString()
+    return new URL(`${getStatusUri(user, status, options)}/activity`).toString()
 }
