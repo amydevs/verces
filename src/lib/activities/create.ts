09b54ca46215c0eb26345605951a03700b94c8bc
amy <ctrlalternativedelete@gmail.com>
2022-12-04 03:29:50 +1100

    added generate note context stuff

---
+++ src/lib/activities/create.ts
@@ -0,0 +1,19 @@
+import { StatusContext } from "./contexts";
+import { type IPost, type ICreate } from "./type";
+
+export const generateCreate = (name: string, domain: string, note: IPost, context = true): ICreate => {
+    const actor = `https://${domain}/users/${name}`;
+    const createMessage: ICreate = {
+        'id': `${note.id}/activity`,
+        'type': 'Create',
+        'actor': actor,
+        'attributedTo': actor,
+        'to': note.to,
+        'cc': note.cc,
+        'object': note,
+    }
+    if (context) {
+        createMessage["@context"] = StatusContext;
+    }
+    return createMessage;
+}
---
+++ src/lib/activities/note.ts
@@ -0,0 +1,79 @@
+import { type IPost } from "./type"
+import { Visibility, type Prisma } from "@prisma/client"
+import { StatusContext } from "./contexts"
+
+export const statusInclude = {
+    include: {
+        replyingTo: {
+            include: {
+                replyingToStatus: true,
+                replyingToUser: true
+            }
+        },
+        mentions: {
+            include: {
+                user: true
+            }
+        }
+    }
+}
+
+type StatusSmall = Prisma.StatusGetPayload<typeof statusInclude>
+
+export const generateNote = (name: string, domain: string, status: StatusSmall, context: true): IPost => {
+    const note: IPost = {
+        'id': `https://${domain}/users/${name}/statuses/${status.id}`,
+        'type': 'Note',
+        'published': status.createdAt.toISOString(),
+        'attributedTo': `https://${domain}/users/${name}`,
+        'content': status.text,
+        'url': `https://${domain}/@/${name}/${status.id}`,
+    }
+    if (context) {
+        note["@context"] = StatusContext;
+    }
+
+    // set replying
+    if (status.replyingTo) {
+        if (status.replyingTo.replyingToStatus.uri) {
+            note.inReplyTo = status.replyingTo.replyingToStatus.url
+        }
+        else {
+            note.inReplyTo = `https://${domain}/users/${status.replyingTo.replyingToUser.name}/statuses/${status.replyingTo.replyingToStatusId}`
+        }             
+    }
+
+    // set to and cc
+    const publicStream = 'https://www.w3.org/ns/activitystreams#Public';
+    const followerStream = `https://${domain}/users/${name}/followers`;
+    if (!Array.isArray(note.to)) {
+        note.cc = []
+    }
+    if (!Array.isArray(note.cc)) {
+        note.cc = []
+    }
+    const mentions = status.mentions.map(e => {
+        const { uri } = e.user;
+        if (uri?.length) {
+            return uri
+        }
+        return `https://${domain}/users/${e.user.name}`;
+    })
+    switch(status.visibility) {
+        case Visibility.Public:
+            note.to = [publicStream, ...mentions];
+            note.cc = [followerStream]
+            break;
+        case Visibility.Unlisted:
+            note.to = [followerStream, ...mentions];
+            note.cc = [publicStream]
+            break;
+        case Visibility.FollowOnly:
+            note.to = [followerStream, ...mentions]
+            break;
+        case Visibility.MentionOnly:
+            note.to = mentions;
+            break;
+    }
+    return note;
+}
