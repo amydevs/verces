b0634112fd5dbbd1b83360a325a47399baad3db8
amy <ctrlalternativedelete@gmail.com>
2022-12-04 05:35:25 +1100

    added tags

---
+++ src/pages/api/ap/users/[userId]/tags.ts
@@ -0,0 +1,7 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+const followers = async (req: NextApiRequest, res: NextApiResponse) => {
+    res.send("WIP");
+};
+
+export default followers;
