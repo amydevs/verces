cef7d11fe9481f61c62892b60b5134fe0e0e2825
amy <ctrlalternativedelete@gmail.com>
2022-12-04 02:48:53 +1100

    added more

---
+++ src/lib/uris.ts
@@ -0,0 +1,27 @@
+import { env } from "env/server.mjs"
+
+const defaultParams = {
+    protocol: "https://",
+    host: env.HOST,
+}
+
+export const getIndexUri = ({ protocol, host } = defaultParams) => {
+    return new URL(`${protocol}${host}`)
+}
+
+export const getUserUri = (user: string, options = defaultParams) => {
+    return new URL(`/users/${user}`, getIndexUri(options))
+}
+
+export const getInboxUri = (user?: string, options = defaultParams) => {
+    if (user) {
+        return new URL('/inbox', getUserUri(user, options))
+    }
+    else {
+        return new URL('/inbox', getIndexUri(options))
+    }
+}
+
+export const getStatusUri = (user: string, status: string, options = defaultParams) => {
+    return new URL(`/statuses/${status}`, getUserUri(user, options))
+}
