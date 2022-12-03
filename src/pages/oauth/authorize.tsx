a9d428103294155e8fe0725685b7a28fb419a954
amy <ctrlalternativedelete@gmail.com>
2022-12-03 23:59:46 +1100

    added authorize button

--- src/pages/oauth/authorize.tsx
+++ src/pages/oauth/authorize.tsx
@@ -1,25 +1,49 @@
 import { type GetServerSideProps, type NextPage } from "next";
-import { useSession } from "next-auth/react";
+import { getProviders, signIn, useSession } from "next-auth/react";
 
 import { trpc } from "utils/trpc";
 import { useRouter } from "next/router";
+import type { ParsedUrlQuery } from "querystring";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+interface AuthorizeProps {
+    query: ParsedUrlQuery;
+    providers: Awaited<ReturnType<typeof getProviders>>
+}
+
+export const getServerSideProps: GetServerSideProps<AuthorizeProps> = async (context) => {
+    const providers = await getProviders();
     return {
         props: {
-            query: context.query
+            query: context.query,
+            providers
         }
     }
 }
-const Authorize: NextPage = () => {
+const Authorize: NextPage<AuthorizeProps> = (props) => {
     const router = useRouter();
     const session = useSession();
-    
+
     const authorize = trpc.oauth.authorize.useMutation();
+    
+    const providers = []
+    for (const key in props.providers) {
+        providers.push(props.providers[key])
+    }
 
     return (
         <>
-            <button onClick={() => authorize.mutate(router.query as any)}></button>
+            {
+                session.data?.user?.id ?
+                <button onClick={() => authorize.mutate(router.query as any)}>Authorize</button>
+                : (() => {
+                    const providers = []
+                    for (const key in props.providers) {
+                        const provider = props.providers[key]
+                        providers.push((<button key={key} onClick={() => signIn(provider?.id)}>Login with {provider?.name}</button>))
+                    }
+                    return providers;
+                })()
+            }
         </>
     );
 };
