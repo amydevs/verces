8763af7fcac2f4127885ddaac409ab9266334692
amy <ctrlalternativedelete@gmail.com>
2022-12-03 15:03:30 +1100

    added router for v1

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
+import { v1Router } from "./v1";
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
+  v1: v1Router
 });
 
 // export type definition of API
---
+++ src/server/trpc/router/v1/index.ts
@@ -0,0 +1,28 @@
+import { env } from "env/server.mjs";
+import { router, publicProcedure, protectedProcedure } from "../../trpc";
+import { version } from '../../../../../package.json'
+import { z } from "zod";
+
+export const v1Router = router({
+    instance: publicProcedure
+        .meta({ openapi: { method: 'GET', path: '/v1/instance' } })
+        .input(z.void())
+        .output(z.object({
+            domain: z.string(),
+            title: z.string(),
+            version: z.string(),
+            source_url: z.string(),
+            description: z.string()
+        }))
+        .query(
+            () => {
+                return {
+                    domain: env.HOST,
+                    title: 'Verces',
+                    version,
+                    source_url: "https://github.com/mastodon/mastodon",
+                    description: "Serverless Activitypub"
+                }
+            }
+        )
+});
