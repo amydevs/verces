be6bf223c02a2307b4ddcf69367482d8c68c3f73
amy <ctrlalternativedelete@gmail.com>
2022-12-06 19:25:01 +1100

    made statuses be parsed using from note

--- src/pages/api/ap/inbox.ts
+++ src/pages/api/ap/inbox.ts
@@ -1,3 +1,4 @@
+import { statusFromNote } from "lib/activities/note";
 import { type IObject, isCreate, isPost } from "lib/activities/type";
 import { getApObjectBody } from "lib/activities/utils";
 import { sendResError } from "lib/errors";
@@ -9,18 +10,14 @@ const inbox = signatureGuard(async (req: NextApiRequest, res: NextApiResponse) =
     if (typeof req.body !== "object") {
         req.body = JSON.parse(req.body);
     }
-    console.log("Inbox: "+req.body);
     const parsed = (await compact(req.body)) as unknown as IObject;
-    console.log("Parsed Inbox: "+parsed);
     
     if (isCreate(parsed)) {
         const body = await getApObjectBody(parsed.object);
         if (!Array.isArray(body) && isPost(body)) {
-            
+            await statusFromNote(body);
         }
     }
-
-    return res.json(parsed);
 });
 
 export default inbox;
