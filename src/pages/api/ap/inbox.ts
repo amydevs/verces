10c27eca6e4c0f41f5864805d39f229f3462d5bb
amy <ctrlalternativedelete@gmail.com>
2022-12-06 22:45:22 +1100

    made transaction work in root

--- src/pages/api/ap/inbox.ts
+++ src/pages/api/ap/inbox.ts
@@ -5,6 +5,7 @@ import { sendResError } from "lib/errors";
 import { signatureGuard } from "lib/guard";
 import { compact } from "lib/jsonld";
 import { type NextApiRequest, type NextApiResponse } from "next";
+import { prisma } from "server/db/client";
 
 const inbox = signatureGuard(async (req: NextApiRequest, res: NextApiResponse) => {
     if (typeof req.body !== "object") {
@@ -15,7 +16,9 @@ const inbox = signatureGuard(async (req: NextApiRequest, res: NextApiResponse) =
     if (isCreate(parsed)) {
         const body = await getApObjectBody(parsed.object);
         if (!Array.isArray(body) && isPost(body)) {
-            await statusFromNote(body);
+            prisma.$transaction(async (prisma) => {
+                await statusFromNote(body, prisma);
+            });
         }
     }
     return res.send("");
