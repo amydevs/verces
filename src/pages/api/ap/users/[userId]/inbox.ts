e23e07d31784b5470fb7dc2c55c6ff00ed6d2a4e
amy <ctrlalternativedelete@gmail.com>
2022-12-06 17:12:17 +1100

    added signature guard to userinbox

--- src/pages/api/ap/users/[userId]/inbox.ts
+++ src/pages/api/ap/users/[userId]/inbox.ts
@@ -1,9 +1,10 @@
+import { signatureGuard } from "lib/guard";
 import { compact } from "lib/jsonld";
 import { type NextApiRequest, type NextApiResponse } from "next";
 
-const inbox = async (req: NextApiRequest, res: NextApiResponse) => {
+const userInbox = signatureGuard(async (req: NextApiRequest, res: NextApiResponse) => {
     const parsed = await compact(JSON.parse(req.body));
     res.send(JSON.stringify(parsed));
-};
+});
 
-export default inbox;
+export default userInbox;
