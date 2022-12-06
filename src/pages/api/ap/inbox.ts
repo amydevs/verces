818ecc19d27d277d1166229cf19b3be482109f4d
amy <ctrlalternativedelete@gmail.com>
2022-12-06 17:23:27 +1100

    added more log to inbox

--- src/pages/api/ap/inbox.ts
+++ src/pages/api/ap/inbox.ts
@@ -9,6 +9,7 @@ const inbox = signatureGuard(async (req: NextApiRequest, res: NextApiResponse) =
     if (!req.body) {
         return sendResError(res, 400);
     }
+    console.log(req.body);
     const parsed = (await compact(req.body)) as unknown as IObject;
     console.log(parsed);
     
