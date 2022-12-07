88edaa61616b007141c640e63785c1da6685dad4
amy <ctrlalternativedelete@gmail.com>
2022-12-08 00:12:51 +1100

    lint next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,84 +7,84 @@
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/.well-known/:path*',
-        destination: '/api/ap/.well-known/:path*'
-      },
-      {
-        source: '/inbox',
-        destination: '/api/ap/inbox',
-      },
-      {
-        source: '/users/:user*',
-        destination: '/api/ap/users/:user*',
-        has: [
-          {
-            type: 'header',
-            key: 'accept',
-            value: '(?<a>.*application\\/activity\\+json.*)'
-          }
-        ]
-      },
-      {
-        source: '/@/:user',
-        destination: '/api/ap/users/:user',
-        has: [
-          {
-            type: 'header',
-            key: 'accept',
-            value: '(?<b>.*application\\/activity\\+json.*)'
-          }
-        ]
-      },
-      {
-        source: '/@/:user/:status*',
-        destination: '/api/ap/users/:user/statuses/:status*',
-        has: [
-          {
-            type: 'header',
-            key: 'accept',
-            value: '(?<b>.*application\\/activity\\+json.*)'
-          }
-        ]
-      }
-    ]
-  },
-  async redirects () {
-    return [
-      {       
-        source: '/users/:user',
-        destination: '/@/:user',
-        has: [
-          {
-            type: 'header',
-            key: 'accept',
-            value: '(?<a>^((?!application\\/activity\\+json).)*$)'
-          }
-        ],
-        permanent: true
-      },
-      {       
-        source: '/users/:user/statuses/:status',
-        destination: '/@/:user/:status',
-        has: [
-          {
-            type: 'header',
-            key: 'accept',
-            value: '(?<a>^((?!application\\/activity\\+json).)*$)'
-          }
-        ],
-        permanent: true
-      }
-    ]
-  }
+    reactStrictMode: true,
+    swcMinify: true,
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/.well-known/:path*",
+                destination: "/api/ap/.well-known/:path*"
+            },
+            {
+                source: "/inbox",
+                destination: "/api/ap/inbox",
+            },
+            {
+                source: "/users/:user*",
+                destination: "/api/ap/users/:user*",
+                has: [
+                    {
+                        type: "header",
+                        key: "accept",
+                        value: "(?<a>.*application\\/activity\\+json.*)"
+                    }
+                ]
+            },
+            {
+                source: "/@/:user",
+                destination: "/api/ap/users/:user",
+                has: [
+                    {
+                        type: "header",
+                        key: "accept",
+                        value: "(?<b>.*application\\/activity\\+json.*)"
+                    }
+                ]
+            },
+            {
+                source: "/@/:user/:status*",
+                destination: "/api/ap/users/:user/statuses/:status*",
+                has: [
+                    {
+                        type: "header",
+                        key: "accept",
+                        value: "(?<b>.*application\\/activity\\+json.*)"
+                    }
+                ]
+            }
+        ];
+    },
+    async redirects () {
+        return [
+            {       
+                source: "/users/:user",
+                destination: "/@/:user",
+                has: [
+                    {
+                        type: "header",
+                        key: "accept",
+                        value: "(?<a>^((?!application\\/activity\\+json).)*$)"
+                    }
+                ],
+                permanent: true
+            },
+            {       
+                source: "/users/:user/statuses/:status",
+                destination: "/@/:user/:status",
+                has: [
+                    {
+                        type: "header",
+                        key: "accept",
+                        value: "(?<a>^((?!application\\/activity\\+json).)*$)"
+                    }
+                ],
+                permanent: true
+            }
+        ];
+    }
 };
 export default config;
