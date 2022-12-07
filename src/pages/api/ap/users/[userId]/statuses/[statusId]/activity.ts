c911daec8951c0ea2c39470ec89202dc0503e119
amy <ctrlalternativedelete@gmail.com>
2022-12-07 12:02:05 +1100

    added restatus to activity

--- src/pages/api/ap/users/[userId]/statuses/[statusId]/activity.ts
+++ src/pages/api/ap/users/[userId]/statuses/[statusId]/activity.ts
@@ -4,22 +4,48 @@ import { generateCreate } from "lib/activities/create";
 import { generateNote, statusInclude } from "lib/activities/note";
 import { prisma } from "server/db/client";
 import { sendResError } from "lib/errors";
+import { Visibility } from "@prisma/client";
 
 const status = async (req: NextApiRequest, res: NextApiResponse) => {
     const { userId, statusId } = req.query;
     if (typeof userId !== "string" || typeof statusId !== "string") {
         return sendResError(res, 400);
     }
+
+    const publicVisibilities = [
+        Visibility.Public,
+        Visibility.Unlisted
+    ];
+
     const foundStatus = await prisma.status.findFirst({
         include: {
             user: true,
             ...statusInclude.include
         },
         where: {
-            user: {
-                name: userId
-            },
-            id: statusId
+            OR: [
+                {
+                    user: {
+                        name: userId
+                    },
+                    id: statusId
+                },
+                {
+                    restatuses: {
+                        some: {
+                            statusId,
+                            user: {
+                                name: userId
+                            },
+                            status: {
+                                visibility: {
+                                    in: publicVisibilities
+                                }
+                            }
+                        },
+                    }
+                }
+            ]           
         }
     });
     if (!foundStatus) {
