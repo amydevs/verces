fac4503561f7f803eeeabc3d7564014c35001990
amy <ctrlalternativedelete@gmail.com>
2022-12-08 19:49:37 +1100

    added generate postHeaders

--- src/lib/activities/utils.ts
+++ src/lib/activities/utils.ts
@@ -1,4 +1,28 @@
 import type { ApObject, IObject } from "./type";
+import crypto from "crypto";
+import { getUserUri } from "lib/uris";
+
+export const ActivityContentType = "application/activity+json"; 
+
+export const generatePostHeaders = (body: string, userIndex: string, privateKey: string, uri: string) => {
+    const url = new URL(uri);
+    const digestHash = crypto.createHash("sha256").update(body).digest("base64");
+    const signer = crypto.createSign("sha256");
+    const date = new Date();
+    const stringToSign = `(request-target): post ${url.pathname}\nhost: ${url.host}\ndate: ${date.toUTCString()}\ndigest: SHA-256=${digestHash}`;
+    signer.update(stringToSign);
+    signer.end();
+    const signature = signer.sign(privateKey);
+    const signature_b64 = signature.toString("base64");
+    const header = `keyId="${getUserUri(userIndex)}",headers="(request-target) host date digest",signature="${signature_b64}"`;
+    const headers = {
+        "Host": url.host,
+        "Date": date.toUTCString(),
+        "Digest": `SHA-256=${digestHash}`,
+        "Signature": header
+    };
+    return headers;
+};
 
 export const getApObjectBody = async (doc: ApObject): Promise<IObject | IObject[]> => {
     if (Array.isArray(doc)) {
@@ -15,7 +39,7 @@ export const getSingleApObjectBody = async (doc: IObject | string): Promise<IObj
     }
     const ftch = await fetch(doc, {
         headers: {
-            "Accept": "application/activity+json",
+            "Accept": ActivityContentType,
         }
     }).then(e => e.json());
     return ftch as IObject;
