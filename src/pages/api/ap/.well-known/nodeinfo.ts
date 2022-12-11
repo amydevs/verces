06b97414680d1bf9965d54a217b95ad9c464b890
amy <ctrlalternativedelete@gmail.com>
2022-12-11 11:08:11 +1100

    node info

---
+++ src/pages/api/ap/.well-known/nodeinfo.ts
@@ -0,0 +1,8 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { env } from "../../../../env/server.mjs";
+
+const nodeInfo = async (req: NextApiRequest, res: NextApiResponse) => {
+
+}
+
+export default nodeInfo;
