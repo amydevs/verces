5f05a08fb8d7641ecc14b540616c76cf21f97f57
amy <ctrlalternativedelete@gmail.com>
2022-12-11 12:08:34 +1100

    added stataus

--- src/server/trpc/router/v1/statuses.ts
+++ src/server/trpc/router/v1/statuses.ts
@@ -1,9 +1,9 @@
 import { Visibility } from "@prisma/client";
-import { publicProcedure, router } from "server/trpc/trpc";
+import { router, protectedProcedure } from "server/trpc/trpc";
 import { z } from "zod";
 
 export const statusesRouter = router({
-    create: publicProcedure
+    create: protectedProcedure
         .meta({ openapi: { method: "POST", path: "/v1/statuses" } })
         .input(z.object({
             status: z.string(),
@@ -13,14 +13,24 @@ export const statusesRouter = router({
                 expires_in: z.number(),
                 multiple: z.boolean().optional(),
                 hide_totals: z.boolean().optional(),
-                in_reply_to_id: z.string().optional(),
-                sensitive: z.boolean().optional(),
-                spoiler_text: z.string().optional(),
-                visibility: z.enum([ "public", "unlisted", "private", "direct" ])
-            }),
-
+            }).optional(),
+            in_reply_to_id: z.string().optional(),
+            sensitive: z.boolean().optional(),
+            spoiler_text: z.string().optional(),
+            visibility: z.enum([ "public", "unlisted", "private", "direct" ]).optional(),
+            language: z.string().optional(),
         }))
-        .mutation(() => {
-            return {};
+        .mutation(({ ctx, input }) => {
+            let visibility;
+            if (input.visibility) {
+                visibility = (input.visibility.charAt(0).toUpperCase + input.visibility.slice(1)) as Visibility;
+            }
+            return ctx.prisma.status.create({
+                data: {
+                    text: input.status,
+                    userId: ctx.session.user.id,
+                    ...({visibility} ?? {}),
+                }
+            });
         })
 });
