9a3f40c66c69d21af7c39c6fb9d119eb554697b0
amy <ctrlalternativedelete@gmail.com>
2022-12-28 10:21:31 +1100

    removed oauth router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,13 +1,11 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
-import { oauthRouter } from "./oauth";
 import { v1Router } from "./v1";
 
 export const appRouter = router({
     example: exampleRouter,
     auth: authRouter,
-    oauth: oauthRouter,
     v1: v1Router
 });
 
