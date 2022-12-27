105f0bde88a73fc118f61a4a4ea787068f534636
amy <ctrlalternativedelete@gmail.com>
2022-12-28 10:30:26 +1100

    added nextcors to oauth

--- src/pages/api/[...trpc].ts
+++ src/pages/api/[...trpc].ts
@@ -10,10 +10,9 @@ import NextCors from "nextjs-cors";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   
     await NextCors(req, res, {
-    // Options
         methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
         origin: "*",
-        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+        optionsSuccessStatus: 200,
     });
 
     // Handle incoming OpenAPI requests
--- src/pages/api/oauth/[...oauth].ts
+++ src/pages/api/oauth/[...oauth].ts
@@ -1,8 +1,14 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 import generateProvider from "server/oauth";
 
 
 const oauth = async (req: NextApiRequest, res: NextApiResponse) => {
+    await NextCors(req, res, {
+        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+        origin: "*",
+        optionsSuccessStatus: 200
+    });
     await (await generateProvider()).callback()(req, res);
 };
 
